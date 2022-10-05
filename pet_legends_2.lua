c4ed793057869fe10d1d0b529aff2d9ea5f2e156
Nalathan <frank.swen@gmail.com>
2022-10-05 12:55:16 +0200

    Update pet_legends_2.lua

--- pet_legends_2.lua
+++ pet_legends_2.lua
@@ -20,10 +20,12 @@ PlayerTab:AddSlider({
 	Name = "WalkSpeed",
 	Min = 18,
 	Max = 200,
-	Default = 5,
+	Default = 18,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
 	ValueName = "Speed",
+    	Save = true,
+    	Flag = "slider",
 	Callback = function(v)
 		hum.WalkSpeed = v
 	end    
@@ -32,8 +34,10 @@ PlayerTab:AddSlider({
 PlayerTab:AddToggle({
 	Name = "Anti AFK",
 	Default = false,
-	Callback = function()
-	game:GetService("Players").LocalPlayer.Idled:connect(function()
+    	Save = true,
+    	Flag = "toggle",
+	Callback = function(Value)
+		game:GetService("Players").LocalPlayer.Idled:connect(function()
         vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
         wait(1)
         vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
