3578a57358fb3b1529dc14b0c2b7a85f2a59dd72
Nalathan <frank.swen@gmail.com>
2022-10-06 00:50:00 +0200

    Update pet_legends_2.lua

--- pet_legends_2.lua
+++ pet_legends_2.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 8864518888 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Pet Legends 2 by Nali Games", HidePremium = true, IntroEnabled = false,SaveConfig = false, ConfigFolder = "Pet Legends 2"})
+local Window = OrionLib:MakeWindow({Name = "Pet Legends 2 by Nalathan", HidePremium = true, IntroEnabled = false,SaveConfig = false, ConfigFolder = "Pet Legends 2"})
 local hum = game:GetService("Players").LocalPlayer.Character.Humanoid
 local vu = game:GetService("VirtualUser")
 
@@ -8,6 +8,32 @@ local TeleportTab = Window:MakeTab({
 	Name = "Teleports",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+
+local RewardTab = Window:MakeTab({
+	Name = "Redeem Reward",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+RewardTab:AddToggle({
+        Name = "Rank Reward",
+	Default = true,
+        Save = true,
+        Flag = "toggle",
+	Callback = function(Value)
+	game:GetService("ReplicatedStorage").Game.Events.ReddemRankReward:InvokeServer()
+	end   
+})
+
+RewardTab:AddToggle({
+        Name = "VIP Reward",
+	Default = true,
+        Save = true,
+        Flag = "toggle",
+	Callback = function(Value)
+	game:GetService("ReplicatedStorage").Game.Events.ReddemVIPReward:InvokeServer()
+	end   
 })
 
 local PlayerTab = Window:MakeTab({
@@ -20,12 +46,12 @@ PlayerTab:AddSlider({
 	Name = "WalkSpeed",
 	Min = 18,
 	Max = 200,
-	Default = 18,
+	Default = 50,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
 	ValueName = "Speed",
-    Save = true,
-    Flag = "slider",
+        Save = true,
+        Flag = "slider",
 	Callback = function(v)
 		hum.WalkSpeed = v
 	end    
@@ -33,11 +59,11 @@ PlayerTab:AddSlider({
 
 PlayerTab:AddToggle({
 	Name = "Anti AFK",
-	Default = false,
-    Save = true,
-    Flag = "toggle",
+	Default = true,
+        Save = true,
+        Flag = "toggle",
 	Callback = function(Value)
-		game:GetService("Players").LocalPlayer.Idled:connect(function()
+	game:GetService("Players").LocalPlayer.Idled:connect(function()
         vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
         wait(1)
         vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
